83e292fd702e540ae6e8986247824ef79414dd2c
ashikmahmud1 <ashikmahmud137@gmail.com>
2017-04-13 16:08:54 +0600

    Recommendation System Changes

--- Server/modules/recommendation/movierecommendation.py
+++ Server/modules/recommendation/movierecommendation.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
-planets = pd.read_excel('../data/MovieData/PlannetData.xlsx')
-print(planets.head())
+column_names = ['user_id','item_id','rating','timestamp']
+ratings = pd.read_csv('C:/Users/ashik/Documents/GitHub/Personal-Assistant/Server/data/recommenderdata/u.data',sep='\t',names=column_names)
+print(ratings.head())
+movie_titles = pd.read_csv('C:/Users/ashik/Documents/GitHub/Personal-Assistant/Server/data/recommenderdata/Movie_Id_Titles')
+print(movie_titles.head())
+ratings = pd.merge(ratings,movie_titles,on='item_id')
+print(ratings.head())
+print(ratings.groupby('title')['rating'].mean())
+movie_matrix = ratings.pivot_table(index = 'user_id',columns = 'title',values = 'rating')
+print(movie_matrix.head())
 
-def KeyWords(self, wordlist):
-    fields = ['Name']
-    df = planets
+starwarsrating = movie_matrix['Star Wars (1977)']
+print(starwarsrating.head())
+
